e738ac87a088d5998ea961dba702ba568c318c49
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-11-22 20:12:58 +0900

    v1.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ object ModConfig {
     const val modId = "jpy"
     const val modName = "JPY"
     const val modLicense = "MIT"
-    const val modVersion = "1.4.1"
+    const val modVersion = "1.5.0"
     const val modGroupId = "io.github.meatwo310.jpy"
     const val modAuthors = "Meatwo310"
     const val modDescription = "Adds JPY items to Minecraft."
